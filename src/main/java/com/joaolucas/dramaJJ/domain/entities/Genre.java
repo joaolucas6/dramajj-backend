d3304f85f979402fa7c50d36485a57b5a1ee84fa
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-12 19:18:30 -0300

    Creating Genre entity

--- src/main/java/com/joaolucas/dramaJJ/domain/entities/Genre.java
+++ src/main/java/com/joaolucas/dramaJJ/domain/entities/Genre.java
@@ -1,4 +1,27 @@
 package com.joaolucas.dramaJJ.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_genre")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    
+    @ManyToMany
+    private List<Drama> dramas = new ArrayList<>();
 }
