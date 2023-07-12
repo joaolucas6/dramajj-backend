321efa96fcce87417fc98b7cb38e28661d71c14f
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-12 19:10:40 -0300

    Creating User entity

--- src/main/java/com/joaolucas/dramaJJ/domain/entities/User.java
+++ src/main/java/com/joaolucas/dramaJJ/domain/entities/User.java
@@ -1,4 +1,55 @@
 package com.joaolucas.dramaJJ.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "tb_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "profile_pic_img_url")
+    private String profilePicImgUrl;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @ManyToOne
+    private List<Review> reviews = new ArrayList<>();
+
+    @ManyToMany
+    private List<User> followers = new ArrayList<>();
+
+    @ManyToMany
+    private List<User> following = new ArrayList<>();
+
+    @ManyToMany
+    private List<Actor> followingActors = new ArrayList<>();
 }
