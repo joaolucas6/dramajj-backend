e961bcbd71df02508fb9f3a3b0a2a58cf62af8e4
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-12 19:15:43 -0300

    Creating Review entity

--- src/main/java/com/joaolucas/dramaJJ/domain/entities/Review.java
+++ src/main/java/com/joaolucas/dramaJJ/domain/entities/Review.java
@@ -1,4 +1,37 @@
 package com.joaolucas.dramaJJ.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_review")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private Drama drama;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "rating")
+    private Date instant;
+
+
 }
