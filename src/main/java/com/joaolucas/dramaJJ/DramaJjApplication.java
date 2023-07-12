ea0541aaf6fe6d3c17a73a5d5d18833463eeb0b1
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-12 18:31:20 -0300

    First commit

---
+++ src/main/java/com/joaolucas/dramaJJ/DramaJjApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.dramaJJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DramaJjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DramaJjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/dramaJJ/DramaJjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.dramaJJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DramaJjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
