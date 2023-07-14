573c5c40efcbfa59b567f1b489dce874ff130624
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-14 19:17:02 -0300

    Specifying exceptions in GenreService

--- src/main/java/com/joaolucas/dramaJJ/services/GenreService.java
+++ src/main/java/com/joaolucas/dramaJJ/services/GenreService.java
@@ -2,6 +2,7 @@ package com.joaolucas.dramaJJ.services;
 
 import com.joaolucas.dramaJJ.domain.dto.GenreDTO;
 import com.joaolucas.dramaJJ.domain.entities.Genre;
+import com.joaolucas.dramaJJ.exceptions.ResourceNotFoundException;
 import com.joaolucas.dramaJJ.repositories.GenreRepository;
 import com.joaolucas.dramaJJ.utils.DTOMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ public class GenreService {
     }
 
     public GenreDTO findById(Long id){
-        return new GenreDTO(genreRepository.findById(id).orElseThrow());
+        return new GenreDTO(genreRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Genre with ID %d was not found", id))));
     }
 
     public GenreDTO create(GenreDTO genreDTO){
@@ -33,7 +34,7 @@ public class GenreService {
     }
 
     public GenreDTO update(Long id, GenreDTO genreDTO){
-        Genre genre = genreRepository.findById(id).orElseThrow();
+        Genre genre = genreRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Genre with ID %d was not found", id)));
         if(genreDTO.getName() != null) genre.setName(genreDTO.getName());
 
         return new GenreDTO(genreRepository.save(genre));
