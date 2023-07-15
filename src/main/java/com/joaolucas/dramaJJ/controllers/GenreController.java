a77f98afd8ac57740e83f2c417157df835b60606
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-15 10:17:38 -0300

    Creating GenreController

---
+++ src/main/java/com/joaolucas/dramaJJ/controllers/GenreController.java
@@ -0,0 +1,47 @@
+package com.joaolucas.dramaJJ.controllers;
+
+
+import com.joaolucas.dramaJJ.domain.dto.ActorDTO;
+import com.joaolucas.dramaJJ.domain.dto.GenreDTO;
+import com.joaolucas.dramaJJ.services.ActorService;
+import com.joaolucas.dramaJJ.services.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/genres")
+public class GenreController {
+    @Autowired
+    private GenreService genreService;
+
+
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> findAll(){
+        return ResponseEntity.ok(genreService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(genreService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<GenreDTO> create(@RequestBody GenreDTO genreDTO){
+        return ResponseEntity.ok(genreService.create(genreDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GenreDTO> update(@PathVariable Long id, @RequestBody GenreDTO genreDTO){
+        return ResponseEntity.ok(genreService.update(id, genreDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        genreService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
