cd3a8c83f85c2eaf2eb5fd043b3e677b37ec1039
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-15 09:55:51 -0300

    Creating ReviewController

---
+++ src/main/java/com/joaolucas/dramaJJ/controllers/ReviewController.java
@@ -0,0 +1,51 @@
+package com.joaolucas.dramaJJ.controllers;
+
+import com.joaolucas.dramaJJ.domain.dto.ReviewDTO;
+import com.joaolucas.dramaJJ.domain.entities.Review;
+import com.joaolucas.dramaJJ.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+
+    @GetMapping
+    public ResponseEntity<List<ReviewDTO>> findAll(){
+        return ResponseEntity.ok(reviewService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReviewDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(reviewService.findById(id));
+    }
+
+    @PostMapping("/{authorId}/{dramaId}")
+    public ResponseEntity<ReviewDTO> create(
+            @PathVariable Long authorId,
+            @PathVariable Long dramaId,
+            @RequestBody ReviewDTO reviewDTO
+    ){
+        return ResponseEntity.ok(reviewService.create(authorId, dramaId, reviewDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReviewDTO> update(@PathVariable Long id, @RequestBody ReviewDTO reviewDTO){
+        return ResponseEntity.ok(reviewService.update(id, reviewDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        reviewService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
