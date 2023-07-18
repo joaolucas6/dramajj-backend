1a35b2cc04f2a967374effe00bfb0e44b2072e18
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-18 11:08:00 -0300

    Adding swagger

---
+++ src/main/java/com/joaolucas/dramaJJ/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.joaolucas.dramaJJ.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+                .title("DramaJJ API")
+                .description("A Spring boot application for a dramas review website")
+                .version("v1")
+        );
+    }
+}
