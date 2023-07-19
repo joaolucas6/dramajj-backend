ecc250aa552fe8d5b83d111fa59a571c3d599e31
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-19 19:25:54 -0300

    Implementing hateoas into GenreService

--- src/main/java/com/joaolucas/dramaJJ/services/GenreService.java
+++ src/main/java/com/joaolucas/dramaJJ/services/GenreService.java
@@ -1,5 +1,6 @@
 package com.joaolucas.dramaJJ.services;
 
+import com.joaolucas.dramaJJ.controllers.GenreController;
 import com.joaolucas.dramaJJ.domain.dto.GenreDTO;
 import com.joaolucas.dramaJJ.domain.entities.Drama;
 import com.joaolucas.dramaJJ.domain.entities.Genre;
@@ -12,6 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class GenreService {
@@ -27,24 +31,42 @@ public class GenreService {
 
         genreRepository.findAll().forEach(genre -> list.add(new GenreDTO(genre)));
 
+        list.forEach(genreDTO -> {
+            genreDTO.add(linkTo(methodOn(GenreController.class).findById(genreDTO.getId())).withSelfRel());
+        });
+
         return list;
     }
 
     public GenreDTO findById(Long id){
-        return new GenreDTO(genreRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Genre with ID %d was not found", id))));
+
+        GenreDTO genreDTO = new GenreDTO(genreRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Genre with ID %d was not found", id))));
+
+        genreDTO.add(linkTo(methodOn(GenreController.class).findById(id)).withSelfRel());
+
+        return genreDTO;
     }
 
     public GenreDTO create(GenreDTO genreDTO){
         Genre genre = DTOMapper.toGenre(genreDTO, List.of());
 
-        return new GenreDTO(genreRepository.save(genre));
+        GenreDTO responseGenreDTO =  new GenreDTO(genreRepository.save(genre));
+
+        responseGenreDTO.add(linkTo(methodOn(GenreController.class).findById(responseGenreDTO.getId())).withSelfRel());
+
+        return responseGenreDTO;
     }
 
     public GenreDTO update(Long id, GenreDTO genreDTO){
         Genre genre = genreRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Genre with ID %d was not found", id)));
         if(genreDTO.getName() != null) genre.setName(genreDTO.getName());
 
-        return new GenreDTO(genreRepository.save(genre));
+        GenreDTO updatedGenreDTO = new GenreDTO(genreRepository.save(genre));
+
+        updatedGenreDTO.add(linkTo(methodOn(GenreController.class).findById(id)).withSelfRel());
+
+
+        return updatedGenreDTO;
     }
 
     public void delete(Long id){
