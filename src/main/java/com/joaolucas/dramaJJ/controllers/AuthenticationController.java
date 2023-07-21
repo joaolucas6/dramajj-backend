cc7bef7c44f0c44d0d983388e15fa85895e793ce
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-21 13:22:52 -0300

    Creating AuthenticationController

---
+++ src/main/java/com/joaolucas/dramaJJ/controllers/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.joaolucas.dramaJJ.controllers;
+
+import com.joaolucas.dramaJJ.domain.dto.AuthenticationRequest;
+import com.joaolucas.dramaJJ.domain.dto.AuthenticationResponse;
+import com.joaolucas.dramaJJ.domain.dto.RegisterRequest;
+import com.joaolucas.dramaJJ.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+            ){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+}
