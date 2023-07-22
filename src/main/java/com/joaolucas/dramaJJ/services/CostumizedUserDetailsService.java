2b2ac620eb871bb936599446a598d800d7f25daf
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-22 19:03:53 -0300

    Refactoring CostumizedUserDetailsService

--- src/main/java/com/joaolucas/dramaJJ/services/CostumizedUserDetailsService.java
+++ src/main/java/com/joaolucas/dramaJJ/services/CostumizedUserDetailsService.java
@@ -2,6 +2,7 @@ package com.joaolucas.dramaJJ.services;
 
 import com.joaolucas.dramaJJ.exceptions.ResourceNotFoundException;
 import com.joaolucas.dramaJJ.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,10 +10,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CostumizedUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
