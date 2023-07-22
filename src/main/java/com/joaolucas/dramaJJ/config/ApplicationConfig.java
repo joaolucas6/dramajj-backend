7965118a576dd8d45e81a19800a253b2a7e63e02
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-22 19:26:10 -0300

    Refactoring ApplicationConfig

--- src/main/java/com/joaolucas/dramaJJ/config/ApplicationConfig.java
+++ src/main/java/com/joaolucas/dramaJJ/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.joaolucas.dramaJJ.config;
 
 import com.joaolucas.dramaJJ.services.CostumizedUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfig {
-
-    @Autowired
-    private CostumizedUserDetailsService costumizedUserDetailsService;
+    
+    private final CostumizedUserDetailsService costumizedUserDetailsService;
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
