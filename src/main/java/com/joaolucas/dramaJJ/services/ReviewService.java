017d0e8cf6cd555d82ceabf7fb939fc922081c57
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-22 18:51:24 -0300

    Refactoring ReviewService

--- src/main/java/com/joaolucas/dramaJJ/services/ReviewService.java
+++ src/main/java/com/joaolucas/dramaJJ/services/ReviewService.java
@@ -10,6 +10,7 @@ import com.joaolucas.dramaJJ.repositories.DramaRepository;
 import com.joaolucas.dramaJJ.repositories.ReviewRepository;
 import com.joaolucas.dramaJJ.repositories.UserRepository;
 import com.joaolucas.dramaJJ.utils.DTOMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,32 +21,33 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
+@RequiredArgsConstructor
 public class ReviewService {
 
-    @Autowired
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private DramaRepository dramaRepository;
+    private final DramaRepository dramaRepository;
 
 
     public List<ReviewDTO> findAll(){
-        List<ReviewDTO> list = new ArrayList<>();
-        reviewRepository.findAll().forEach(review -> list.add(new ReviewDTO(review)));
+        List<ReviewDTO> allReviewsDTO = new ArrayList<>();
+        reviewRepository.findAll().forEach(
+                review -> allReviewsDTO.add(new ReviewDTO(review))
+        );
 
-        list.forEach(reviewDTO -> {
+        allReviewsDTO.forEach(reviewDTO -> {
             reviewDTO.add(linkTo(methodOn(ReviewController.class).findById(reviewDTO.getId())).withSelfRel());
         });
 
-        return list;
+        return allReviewsDTO;
     }
 
     public ReviewDTO findById(Long id){
-
-        ReviewDTO reviewDTO = new ReviewDTO(reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Review with ID %d was not found", id))));
+        ReviewDTO reviewDTO = new ReviewDTO(
+                reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Review with ID %d was not found", id)))
+        );
 
         reviewDTO.add(linkTo(methodOn(ReviewController.class).findById(id)).withSelfRel());
 
@@ -53,10 +55,12 @@ public class ReviewService {
     }
 
     public ReviewDTO create(Long authorId, Long dramaId, ReviewDTO reviewDTO){
-
-
-        User user = userRepository.findById(authorId).orElseThrow(() -> new ResourceNotFoundException(String.format("User with ID %d was not found", authorId)));
-        Drama drama = dramaRepository.findById(dramaId).orElseThrow(() -> new ResourceNotFoundException(String.format("Drama with ID %d was not found", dramaId)));
+        User user = userRepository.findById(authorId).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("User with ID %d was not found", authorId))
+        );
+        Drama drama = dramaRepository.findById(dramaId).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("Drama with ID %d was not found", dramaId))
+        );
         Review review = DTOMapper.toReview(reviewDTO, user, drama);
 
         user.getReviews().add(review);
@@ -70,8 +74,9 @@ public class ReviewService {
     }
 
     public ReviewDTO update(Long reviewId, ReviewDTO reviewDTO){
-
-        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new ResourceNotFoundException(String.format("Review with ID %d was not found", reviewId)));
+        Review review = reviewRepository.findById(reviewId).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("Review with ID %d was not found", reviewId))
+        );
 
         if(reviewDTO.getTitle() != null) review.setTitle(reviewDTO.getTitle());
         if(reviewDTO.getText() != null) review.setText(reviewDTO.getText());
@@ -87,8 +92,9 @@ public class ReviewService {
     }
 
     public void delete(Long id){
-
-        Review review = reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Review with ID %d was not found", id)));
+        Review review = reviewRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("Review with ID %d was not found", id))
+        );
 
         User author = review.getAuthor();
         Drama drama = review.getDrama();
