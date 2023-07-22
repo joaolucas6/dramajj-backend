1c33d9c34c83942bec2f3f369e2d4c955a865caa
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-22 19:25:11 -0300

    Refactoring UserController

--- src/main/java/com/joaolucas/dramaJJ/controllers/UserController.java
+++ src/main/java/com/joaolucas/dramaJJ/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.joaolucas.dramaJJ.domain.dto.ActorDTO;
 import com.joaolucas.dramaJJ.domain.dto.DramaDTO;
 import com.joaolucas.dramaJJ.domain.dto.UserDTO;
 import com.joaolucas.dramaJJ.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll(){
