9cba7586fd916d0125ffb1cd19fa48b49736780b
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-07-22 19:16:07 -0300

    Refactoring ExceptionResponse and GlobalExceptionHandler

--- src/main/java/com/joaolucas/dramaJJ/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/joaolucas/dramaJJ/exceptions/handler/GlobalExceptionHandler.java
@@ -4,12 +4,9 @@ import com.joaolucas.dramaJJ.exceptions.ConflictException;
 import com.joaolucas.dramaJJ.exceptions.ExceptionResponse;
 import com.joaolucas.dramaJJ.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
@@ -19,39 +16,40 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleGenericException(Exception exception){
 
-        ExceptionResponse response = new ExceptionResponse();
-        response.setTimestamp(new Date());
-        response.setError(HttpStatus.INTERNAL_SERVER_ERROR.name());
-        response.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        response.setMessage(exception.getMessage());
-
-        return ResponseEntity.status(response.getErrorCode()).body(response);
+        ExceptionResponse response = new ExceptionResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.name(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                exception.getMessage(),
+                new Date()
+        );
+        return ResponseEntity.status(response.errorCode()).body(response);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleResourceNotFoundException(ResourceNotFoundException exception){
 
-        ExceptionResponse response = new ExceptionResponse();
+        ExceptionResponse response = new ExceptionResponse(
+                HttpStatus.NOT_FOUND.name(),
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(),
+                new Date()
+        );
 
-        response.setTimestamp(new Date());
-        response.setError(HttpStatus.NOT_FOUND.name());
-        response.setErrorCode(HttpStatus.NOT_FOUND.value());
-        response.setMessage(exception.getMessage());
 
-        return ResponseEntity.status(response.getErrorCode()).body(response);
+        return ResponseEntity.status(response.errorCode()).body(response);
     }
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ExceptionResponse> handleConflictException(ConflictException exception){
 
-        ExceptionResponse response = new ExceptionResponse();
+        ExceptionResponse response = new ExceptionResponse(
+                HttpStatus.CONFLICT.name(),
+                HttpStatus.CONFLICT.value(),
+                exception.getMessage(),
+                new Date()
+        );
 
-        response.setTimestamp(new Date());
-        response.setError(HttpStatus.CONFLICT.name());
-        response.setErrorCode(HttpStatus.CONFLICT.value());
-        response.setMessage(exception.getMessage());
-
-        return ResponseEntity.status(response.getErrorCode()).body(response);
+        return ResponseEntity.status(response.errorCode()).body(response);
     }
 
 }
